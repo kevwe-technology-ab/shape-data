9f709ab7477cfaa7c54c993dcda1223edfd98cc9
Paolo Lulli <paolo@lulli.net>
2021-04-11 18:20:52 +0200

    fixing sonar issues

--- api/src/main/java/net/lulli/shape/data/api/Dialect.java
+++ api/src/main/java/net/lulli/shape/data/api/Dialect.java
@@ -1,12 +1,22 @@
 package net.lulli.shape.data.api;
 
 public enum Dialect {
-  STANDARD,
-  ORACLE,
-  MYSQL,
-  POSTGRES,
-  SYBASE,
-  MSSQL,
-  SQLITE,
-  CASSANDRA
+	STANDARD("standard"), 
+	ORACLE("oracle"), 
+	MYSQL("mysql"), 
+	POSTGRES("postgres"), 
+	SYBASE("sybase"), 
+	MSSQL("mssql"),
+	SQLITE("sqlite"), 
+	CASSANDRA("cassandra");
+
+	private String dialectString;
+
+	Dialect(String dialect) {
+		this.dialectString = dialect;
+	}
+
+	String count() {
+		return dialect;
+	}
 }
